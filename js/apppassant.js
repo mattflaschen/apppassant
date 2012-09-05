cb1b055324139a349b5ccdc030b5f0e498765871
Matthew Flaschen <matthew.flaschen@gatech.edu>
2012-09-04 20:10:50 -0400

    Link hashtags and mentions.

--- js/apppassant.js
+++ js/apppassant.js
@@ -69,6 +69,20 @@
 		var $controlHolder = $('<div />', {'class': 'controls'}).append(controls);
 
 		var $msg = $('<p/>', {html: html});
+		$('span[itemprop=hashtag]').each(function()
+		{
+			var $this = $(this);
+			var hashtag = $this.data('hashtag-name');
+			$this.html($('<a />', {href: 'http://appeio.com/?tag=' + hashtag, text: $this.text()}));
+		});
+
+		$('span[itemprop=mention]').each(function()
+		{
+			var $this = $(this);
+			var mention = $this.data('mention-name');
+			$this.html($('<a />', {href: 'http://appeio.com/' + mention, text: $this.text()}));
+		});
+
 		$boardControlHolder.html('').append($boardHolder, $controlHolder, $msg, $('<hr />'));
 		var board = $boardHolder.chess({pgn: pgn});
 		gotoEnd();
